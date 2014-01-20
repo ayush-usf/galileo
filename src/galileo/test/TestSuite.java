84db11bb5a34547e3bd934ad067ce02cbdc2985e
Matthew Malensek <matt@malensek.net>
2014-01-20 00:58:24 -0700

    junit test suite (for future ant build automation)

---
+++ src/galileo/test/TestSuite.java
@@ -0,0 +1,16 @@
+package galileo.test;
+
+import galileo.test.dataset.feature.*;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    ArrayTest.class,
+    Casts.class,
+    Serialization.class,
+
+})
+public class TestSuite { }
