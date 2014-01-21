0d7fda2e5f51de26b9106d460ab2cd36eafde4cd
Matthew Malensek <matt@malensek.net>
2014-01-20 17:37:11 -0700

    Implement SimpleMap

--- src/galileo/dataset/feature/FeatureArraySet.java
+++ src/galileo/dataset/feature/FeatureArraySet.java
@@ -25,11 +25,15 @@ software, even if advised of the possibility of such damage.
 
 package galileo.dataset.feature;
 
+import galileo.dataset.SimpleMap;
 import galileo.serialization.ByteSerializable;
+
+import galileo.serialization.SerializationException;
 import galileo.serialization.SerializationInputStream;
 import galileo.serialization.SerializationOutputStream;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -40,16 +44,19 @@ import java.util.Map;
  * @author malensek
  */
 public class FeatureArraySet
-implements ByteSerializable, Iterable<FeatureArray> {
+implements ByteSerializable, Iterable<FeatureArray>,
+        SimpleMap<String, FeatureArray> {
 
     private Map<String, FeatureArray> arrays = new HashMap<>();
 
     public FeatureArraySet() { }
 
+    @Override
     public void put(FeatureArray array) {
         arrays.put(array.getName(), array);
     }
 
+    @Override
     public FeatureArray get(String name) {
         return arrays.get(name);
     }
@@ -59,22 +66,25 @@ implements ByteSerializable, Iterable<FeatureArray> {
         return arrays.values().iterator();
     }
 
+    @Override
+    public Collection<FeatureArray> values() {
+        return arrays.values();
+    }
+
+    @Override
+    public int size() {
+        return arrays.size();
+    }
+
     @Deserialize
     public FeatureArraySet(SerializationInputStream in)
-    throws IOException {
-        int numArrays = in.readInt();
-        for (int i = 0; i < numArrays; ++i) {
-            FeatureArray array = new FeatureArray(in);
-            put(array);
-        }
+    throws IOException, SerializationException {
+        in.readSimpleMap(FeatureArray.class, this);
     }
 
     @Override
     public void serialize(SerializationOutputStream out)
     throws IOException {
-        out.writeInt(arrays.size());
-        for (FeatureArray array : arrays.values()) {
-            out.writeSerializable(array);
-        }
+        out.writeSimpleMap(this);
     }
 }
