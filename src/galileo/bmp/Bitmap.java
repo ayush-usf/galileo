8165966c347c6f237a16e421697ac257a1daeb0a
Matthew Malensek <matt@malensek.net>
2014-03-24 17:31:52 -0600

    Create bitmap interface

--- src/galileo/bmp/Bitmap.java
+++ src/galileo/bmp/Bitmap.java
@@ -25,56 +25,21 @@ software, even if advised of the possibility of such damage.
 
 package galileo.bmp;
 
-import com.googlecode.javaewah.EWAHCompressedBitmap;
-
 /**
- * A thin wrapper around {@link com.googlecode.javaewah.EWAHCompressedBitmap}
- * to enable us to decouple from a particular bitmap implementation in the
- * future if need be.
+ * An interface for bitmap indexes.
  *
  * @author malensek
  */
-public class Bitmap {
-
-    private EWAHCompressedBitmap bmp;
-
-    public Bitmap() {
-        this.bmp = new EWAHCompressedBitmap();
-    }
-
-    private Bitmap(EWAHCompressedBitmap bmp) {
-        this.bmp = bmp;
-    }
+public interface Bitmap<T> {
 
     /**
-     * Sets the specified bit(s) in the BitArray.
+     * Sets the specified bit(s) in the index.
      *
      * @param bits list of bits to set (as in, set to 1).
      *
-     * @throws BitArrayException if the bits could not be set.
+     * @throws BitmapException if the bits could not be set.
      */
-    public void set(int... bits)
-    throws BitmapException {
-        for (int i : bits) {
-            if (bmp.set(i) == false) {
-                throw new BitmapException("Could not set bit at " + i);
-            }
-        }
-    }
+    public void set(int... bits) throws BitmapException;
 
-    /**
-     * Performs a logical AND operation between two Bitmap instances.
-     */
-    public Bitmap and(Bitmap otherBitmap)
-    throws BitmapException {
-        return new Bitmap(this.bmp.and(otherBitmap.bmp));
-    }
-
-    /**
-     * Performs a logical OR operation between two Bitmap instances.
-     */
-    public Bitmap or(Bitmap otherBitmap)
-    throws BitmapException {
-        return new Bitmap(this.bmp.or(otherBitmap.bmp));
-    }
+    public boolean intersects(Bitmap<T> otherBitmap) throws BitmapException;
 }
