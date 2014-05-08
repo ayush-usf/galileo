ae44a8c32b21cbf2f8f0210c5d8fedf80eff7374
Matthew Malensek <matt@malensek.net>
2014-05-07 18:23:41 -0600

    Make EventWrapper an interface

--- src/galileo/event/EventWrapper.java
+++ src/galileo/event/EventWrapper.java
@@ -1,28 +1,52 @@
+/*
+Copyright (c) 2014, Colorado State University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are
+disclaimed. In no event shall the copyright holder or contributors be liable for
+any direct, indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused and on
+any theory of liability, whether in contract, strict liability, or tort
+(including negligence or otherwise) arising in any way out of the use of this
+software, even if advised of the possibility of such damage.
+*/
 
 package galileo.event;
-
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
 
 import java.io.IOException;
 
 import galileo.net.GalileoMessage;
-import galileo.serialization.SerializationOutputStream;
 
-public class EventWrapper {
+/**
+ * Defines how Events are wrapped and unwrapped for serialization and
+ * transmission across the network.
+ *
+ * @author malensek
+ */
+public interface EventWrapper {
 
-    public static GalileoMessage wrap(Event e)
-    throws IOException {
-        ByteArrayOutputStream bOut = new ByteArrayOutputStream();
-        SerializationOutputStream sOut = new SerializationOutputStream(
-                new BufferedOutputStream(bOut));
+    /**
+     * Wraps an {@link Event} up in a {@link GalileoMessage}, ready to be
+     * transmitted across the network.
+     */
+    public GalileoMessage wrap(Event e) throws IOException;
 
-        sOut.writeInt(e.getTypeMap().toInt());
-        sOut.writeSerializable(e);
-        sOut.close();
+    /**
+     * Unwraps a {@link GalileoMessage} to extract the {@link Event} it
+     * contains.
+     */
+    public Event unwrap(GalileoMessage msg);
 
-        byte[] payload = bOut.toByteArray();
-        GalileoMessage msg = new GalileoMessage(payload);
-        return msg;
-    }
 }
