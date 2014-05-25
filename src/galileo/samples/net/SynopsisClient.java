a6cdc7d7a4f0cc5b20543a68dc5eebbc821743de
Matthew Malensek <matt@malensek.net>
2014-05-25 00:45:24 -0600

    Synopsis test client and server

---
+++ src/galileo/samples/net/SynopsisClient.java
@@ -0,0 +1,100 @@
+package galileo.samples.net;
+
+import java.io.IOException;
+
+import java.util.Random;
+
+import galileo.event.ConcurrentEventReactor;
+import galileo.event.EventContext;
+import galileo.event.EventHandler;
+import galileo.event.EventReactor;
+import galileo.event.EventWithSynopsis;
+import galileo.event.SynopsisWrapper;
+import galileo.net.ClientMessageRouter;
+import galileo.net.NetworkDestination;
+
+public class SynopsisClient {
+
+    private NetworkDestination server;
+
+    private ClientMessageRouter messageClient;
+    private ConcurrentEventReactor eventReactor;
+
+    public SynopsisClient(NetworkDestination server)
+    throws IOException {
+        this.server = server;
+        eventReactor = new ConcurrentEventReactor(
+                this, new SynopsisWrapper(), 1);
+        eventReactor.start();
+        messageClient = new ClientMessageRouter();
+        messageClient.addListener(eventReactor);
+    }
+
+    public void sendMessages()
+    throws IOException {
+        Random random = new Random();
+
+        /* Send a simple, empty message */
+        EventWithSynopsis event1 = new EventWithSynopsis(
+                "SynopsisA", new byte[3]);
+        messageClient.sendMessage(server, eventReactor.wrapEvent(event1));
+
+        /* How about some random data? */
+        byte[] data = new byte[8192];
+        random.nextBytes(data);
+        EventWithSynopsis event2 = new EventWithSynopsis("SynopsisA", data);
+        messageClient.sendMessage(server, eventReactor.wrapEvent(event2));
+
+
+        /* Send some random bytes on SynopsisB.  The server will print out the
+         * hex representation of the data. */
+        byte[] smallData = new byte[100];
+        random.nextBytes(smallData);
+        EventWithSynopsis event3
+            = new EventWithSynopsis("SynopsisB", smallData);
+        messageClient.sendMessage(server, eventReactor.wrapEvent(event3));
+
+        /* Send something on SynopsisC.  This should make the server send us a
+         * reply. */
+        EventWithSynopsis event4
+            = new EventWithSynopsis("SynopsisC", new byte[1]);
+        messageClient.sendMessage(server, eventReactor.wrapEvent(event4));
+
+        /* Send something on a special synopsis, 'ShutdownRequest'.  This will
+         * make the server send us a 'Shutdown' reply that we will obey */
+        EventWithSynopsis event5
+            = new EventWithSynopsis("ShutdownRequest", new byte[1]);
+        messageClient.sendMessage(server, eventReactor.wrapEvent(event5));
+    }
+
+    @EventHandler
+    public void processEvent(EventWithSynopsis event, EventContext context)
+    throws IOException {
+        String synopsis = event.getSynopsis();
+
+        System.out.println("We got a reply from the server!");
+        System.out.println("Synopsis: " + synopsis);
+
+        if (synopsis.equals("Shutdown")) {
+            System.out.println("Goodbye!");
+            messageClient.shutdown();
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args)
+    throws Exception {
+        if (args.length < 1) {
+            System.out.println("Usage: galileo.samples.net.SynopsisClient "
+                    + "<server hostname>");
+            System.exit(1);
+        }
+
+        String hostname = args[0];
+        NetworkDestination server = new NetworkDestination(
+                hostname, SynopsisServer.DEFAULT_PORT);
+
+        SynopsisClient client = new SynopsisClient(server);
+        client.sendMessages();
+    }
+}
---
+++ src/galileo/samples/net/SynopsisServer.java
@@ -0,0 +1,114 @@
+/*
+Copyright (c) 2014, Colorado State University
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are
+disclaimed. In no event shall the copyright holder or contributors be liable for
+any direct, indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused and on
+any theory of liability, whether in contract, strict liability, or tort
+(including negligence or otherwise) arising in any way out of the use of this
+software, even if advised of the possibility of such damage.
+*/
+
+package galileo.samples.net;
+
+import java.io.IOException;
+
+import java.math.BigInteger;
+
+import galileo.event.EventContext;
+import galileo.event.EventHandler;
+import galileo.event.EventReactor;
+import galileo.event.EventWithSynopsis;
+import galileo.event.SynopsisWrapper;
+import galileo.net.ServerMessageRouter;
+
+public class SynopsisServer {
+
+    public static final int DEFAULT_PORT = 7777;
+
+    private int port;
+
+    private ServerMessageRouter messageRouter;
+    private EventReactor eventReactor;
+
+    public SynopsisServer(int port) {
+        this.port = port;
+    }
+
+    public void start()
+    throws IOException {
+        System.out.println("Starting up.");
+        eventReactor = new EventReactor(this, new SynopsisWrapper());
+        messageRouter = new ServerMessageRouter(this.port);
+        messageRouter.addListener(eventReactor);
+        messageRouter.listen();
+        System.out.println("Listening on port " + this.port);
+
+        /* Process events as they come in */
+        while (true) {
+            try {
+                eventReactor.processNextEvent();
+            } catch (Exception e) {
+                System.out.println("Error processing event!");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @EventHandler
+    public void processEvent(EventWithSynopsis event, EventContext context)
+    throws IOException {
+        String synopsis = event.getSynopsis();
+        byte[] payload = event.getPayload();
+
+        System.out.println("Got an event! Synopsis: " + synopsis);
+
+        if (synopsis.equals("SynopsisA")) {
+
+            System.out.println("Its length is: " + payload.length);
+
+        } else if (synopsis.equals("SynopsisB")) {
+
+            BigInteger bi = new BigInteger(payload);
+            System.out.println("Hex representation: " + bi.toString(16));
+
+        } else if (synopsis.equals("SynopsisC")) {
+
+            System.out.println("String representation: " + new String(payload));
+            System.out.println("We will send a reply.");
+            EventWithSynopsis reply = new EventWithSynopsis(
+                    "ReplySynopsis", "Hello World!".getBytes());
+            context.write(reply);
+
+        } else if (synopsis.equals("ShutdownRequest")) {
+
+            EventWithSynopsis reply = new EventWithSynopsis(
+                    "Shutdown", new byte[0]);
+            context.write(reply);
+
+        } else {
+            System.out.println("I don't know what to do with this "
+                + "message synopsis!");
+        }
+    }
+
+    public static void main(String[] args)
+    throws IOException {
+        SynopsisServer server = new SynopsisServer(DEFAULT_PORT);
+        server.start();
+    }
+}
