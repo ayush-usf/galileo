6be29eec382e35a363b9fd180ca694fb241a624c
Matthew Malensek <matt@malensek.net>
2014-05-28 23:46:44 -0600

    Add placeholder for ClientServer sample

---
+++ src/galileo/samples/net/ClientServer.java
@@ -0,0 +1,14 @@
+package galileo.samples.net;
+
+import galileo.net.DualMessageRouter;
+
+public class ClientServer {
+
+    private DualMessageRouter messageRouter;
+
+    public ClientServer() { }
+
+    public static void main(String[] args) {
+
+    }
+}
