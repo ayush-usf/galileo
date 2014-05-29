4f6c5823eb9d8307e2af2a0ea6ff33aab64e2c8f
Matthew Malensek <matt@malensek.net>
2014-05-28 23:55:40 -0600

    More implementation

--- src/galileo/samples/net/ClientServer.java
+++ src/galileo/samples/net/ClientServer.java
@@ -4,9 +4,21 @@ import galileo.net.DualMessageRouter;
 
 public class ClientServer {
 
+    public static final int DEFALT_PORT = 7777;
+
+    private int port;
     private DualMessageRouter messageRouter;
 
-    public ClientServer() { }
+    public ClientServer() {
+        this(DEFALT_PORT);
+    }
+
+    public ClientServer(int port) {
+        this.port = port;
+    }
+
+    public void listen() {
+    }
 
     public static void main(String[] args) {
 
