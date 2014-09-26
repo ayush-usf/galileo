b532397d9f9a0e9231bea4d53b3a4da60d051dbc
Matthew Malensek <matt@malensek.net>
2014-09-26 13:02:44 -0600

    Change equals() behavior, add hashCode()

--- src/galileo/dataset/feature/ByteArray.java
+++ src/galileo/dataset/feature/ByteArray.java
@@ -25,6 +25,8 @@ software, even if advised of the possibility of such damage.
 
 package galileo.dataset.feature;
 
+import java.util.Arrays;
+
 public class ByteArray implements Comparable<ByteArray> {
 
     private byte[] array;
@@ -38,21 +40,29 @@ public class ByteArray implements Comparable<ByteArray> {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(array);
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-
         if (obj == null) {
             return false;
         }
-
         if (getClass() != obj.getClass()) {
             return false;
         }
-
-        ByteArray that = (ByteArray) obj;
-        return (compareTo(that) == 0);
+        ByteArray other = (ByteArray) obj;
+        if (!Arrays.equals(array, other.array)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
