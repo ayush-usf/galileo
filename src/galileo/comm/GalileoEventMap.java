18099ed584581488e95886be0be04b128a5b6936
Matthew Malensek <matt@malensek.net>
2014-10-14 16:51:33 -0600

    Update event mappings

--- src/galileo/comm/GalileoEventMap.java
+++ src/galileo/comm/GalileoEventMap.java
@@ -4,12 +4,14 @@ import galileo.event.EventMap;
 
 public class GalileoEventMap extends EventMap {
     public GalileoEventMap() {
-        addMapping(StorageEvent.class);
-        addMapping(StorageRequest.class);
+        addMapping(10, DebugEvent.class);
 
-        addMapping(QueryEvent.class);
-        addMapping(QueryRequest.class);
-        addMapping(QueryPreamble.class);
-        addMapping(QueryResponse.class);
+        addMapping(100, StorageEvent.class);
+        addMapping(101, StorageRequest.class);
+
+        addMapping(200, QueryEvent.class);
+        addMapping(201, QueryRequest.class);
+        addMapping(202, QueryPreamble.class);
+        addMapping(203, QueryResponse.class);
     }
 }
