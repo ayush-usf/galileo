350aceb8e37566e8af7fe2b59336dd302c17146e
Matthew Malensek <matt@malensek.net>
2016-07-24 16:24:12 -0600

    Move SimplePair to dataset

--- src/main/java/edu/colostate/cs/galileo/dataset/SimplePair.java
+++ src/main/java/edu/colostate/cs/galileo/dataset/SimplePair.java
@@ -23,7 +23,7 @@ any theory of liability, whether in contract, strict liability, or tort
 software, even if advised of the possibility of such damage.
 */
 
-package io.sigpipe.sing.dataset;
+package edu.colostate.cs.galileo.dataset;
 
 /**
  * Represents a {@link Pair} that contains two items of the same type.
